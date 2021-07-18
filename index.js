fac55012d019e3d676b533f402fdd57ac0e85846
ashok07iet <ashok07iet@hotmail.com>
2021-07-18 21:39:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hostname = "localhost";
 const port = process.env.PORT|8080;
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World4!');
+  response.end('Hello World6!');
 }).listen(port);
 
 console.log(`Server running at http://${hostname}:${port}`);
